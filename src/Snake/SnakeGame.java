99cf9f6fa818451d4460676e9a6ccbcaac51fe5f
max <nzalimaxime1@gmail.com>
2025-05-15 14:51:27 +0200

    adding constructor of Gameframe to display all the information on the screen

--- src/Snake/SnakeGame.java
+++ src/Snake/SnakeGame.java
@@ -1,9 +1,12 @@
 package Snake;
+import javax.swing.*;
 
 public class SnakeGame {
     public static void main(String[] args) {
         // first we create an instance of GameFrame 2 ways to do it
        // first way  GameFrame frame = new GameFrame();
+        // this to call all the information that come from GameFrame
         new GameFrame();
+
     }
 }
