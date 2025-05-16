eb33743f932e00852a33a8670e2aef5d685de240
maxime Nzali <nzalimaxime1@gmailcom>
2025-05-16 12:38:40 +0200

    creation methode start game , paint component , draw, key pressed

--- src/Snake/GamePanel.java
+++ src/Snake/GamePanel.java
@@ -24,7 +24,9 @@ public class GamePanel extends JPanel implements ActionListener {
 
     public class MyKeyAdapter extends KeyAdapter {
         @Override
-        public void keyPressed(KeyEvent e){}
+        public void keyPressed(KeyEvent e){
+
+        }
     }
     GamePanel (){
         random = new Random();
@@ -40,19 +42,100 @@ public class GamePanel extends JPanel implements ActionListener {
         timer = new Timer(DELAY, this); // crée un timer qui appelle actionPerformed() à chaque "tick"
         timer.start();
     }
-    public void paintCoponent(Graphics g){
+    public void paintComponent(Graphics g){
         super.paintComponent(g); //
         draw(g);
     }
     public void draw (Graphics g){
         g.setColor(Color.WHITE);
         g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+        for (int i = 0; i < bodyParts; i++){
+            if (i ==0){
+                g.setColor(Color.green);
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            }
+            else {
+                g.setColor(new Color(45, 180, 0));
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE); // corps
+            }
+        }
 
 
     }
-    public void move (){}
-    public void checkApple(){}
-    public void gameOver(Graphics g){}
+    public void checkCollisions() {
+        // Checks if head collides with body
+        for (int i = bodyParts; i > 0; i--) {
+            if ((x[0] == x[i]) && (y[0] == y[i])) {
+                running = false;
+            }
+        }
+        // Check if head touches left border
+        if (x[0] < 0) {
+            running = false;
+        }
+        // Check if head touches right border
+        if (x[0] > WIDTH) {
+            running = false;
+        }
+        // Check if head touches top border
+        if (y[0] < 0) {
+            running = false;
+        }
+        // Check if head touches bottom border
+        if (y[0] > HEIGHT) {
+            running = false;
+        }
+
+        if (!running) {
+            timer.stop();
+        }
+    }
+
+    public void move (){
+        for (int i = bodyParts; i > 0; i--) {
+            x[i] = x[i - 1];
+            y[i] = y[i - 1];
+        }
+        switch (direction) {
+            case 'U':
+                y[0] = y[0] - UNIT_SIZE;
+                break;
+            case 'D':
+                y[0] = y[0] + UNIT_SIZE;
+                break;
+            case 'L':
+                x[0] = x[0] - UNIT_SIZE;
+                break;
+            case 'R':
+                x[0] = x[0] + UNIT_SIZE;
+                break;
+        }
+    }
+    public void checkApple(){
+        if ((x[0] == appleX) && (y[0] == appleY)) {
+            bodyParts++;
+            applesEaten++;
+            newApple();
+        }
+    }
+    public void gameOver(Graphics g){
+
+        // Score
+        g.setColor(Color.MAGENTA); // Texte rouge
+        g.setFont(new Font("Ink Free", Font.BOLD, 70)); // Police utilisée pour le score
+        FontMetrics metrics1 = getFontMetrics(g.getFont()); // Sert à mesurer la largeur du texte
+        g.drawString("Score: " + applesEaten,
+                (WIDTH - metrics1.stringWidth("Score: " + applesEaten)) / 2,
+                g.getFont().getSize());
+        //game over
+        g.setColor(Color.RED);
+        g.setFont(new Font("Ink Free", Font.BOLD, 100));
+        FontMetrics metrics2 = getFontMetrics(g.getFont());
+        g.drawString("Game Over",
+                (WIDTH - metrics2.stringWidth("Game Over")) / 2,
+                HEIGHT / 2);
+
+    }
     public void newApple() {
         appleX = random.nextInt((int)(WIDTH / UNIT_SIZE)) * UNIT_SIZE;
         appleY = random.nextInt((int)(HEIGHT / UNIT_SIZE)) * UNIT_SIZE;
